739650135db1b376f83c9cb74ebe0b7b15763854
as-segaf <ziyanassegaf20@gmail.com>
2019-12-09 20:52:56 +0700

    team detail page

--- football-info-pwa/js/api.js
+++ football-info-pwa/js/api.js
@@ -26,7 +26,8 @@ function error(error) {
 }
 
 
-
+function getStandings() {
+    
 fetch(url, {
     method: 'GET',
     headers: {
@@ -36,7 +37,7 @@ fetch(url, {
 .then(status)
 .then(json)
 .then(function(data) {
-    var tableHTML = `
+    var tableData = `
         <div class='list'>
             <table class='highlight responsive-table'>
                 <thead>
@@ -56,19 +57,54 @@ fetch(url, {
     var listClub = '';
     data.standings[0].table.forEach(function(standings) {
         listClub += `
-        <tr>
+        <tr style="cursor:pointer" onclick="location.href='./team.html?id=${standings.team.id}'">
             <td>${standings.position}</td>
             <td>${standings.team.name}</td>
             <td>${standings.points}</td>
         </tr>
+        </a>
         `;
 
     });
     // sisipkan 
-    console.log(data.standings[0].table);
-    document.getElementById("body-content").innerHTML = tableHTML;  
+    // console.log(data.standings[0].table);
+    document.getElementById("body-content").innerHTML = tableData;  
     document.getElementById("tes").innerHTML = listClub;  
     
     
 })
 .catch(error);
+}
+
+
+function getTeam() {
+    //ambili nilai query parameter(id)
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get('id');
+
+    fetch('https://api.football-data.org/v2/teams/' + idParam, {
+        method: 'GET',
+        headers:{
+            'X-Auth-Token' : '5384ecbdad5449188d971b6cc568f33a'
+        }
+    })
+    .then(status)
+    .then(json)
+    .then(function(data) {
+        var teamHTML = `
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img src="${data.crestUrl}" />
+            </div>
+            <div class="card-content">
+              <span class="card-title">${data.name}</span>
+              <p>Stadium : ${data.venue}</p>
+              <p>Address : ${data.address}</p>
+              <p>Website : <a href='${data.website}'>${data.website}</a></p>
+            </div>
+          </div>
+        `;
+        console.log(teamHTML);
+        document.getElementById('body-content').innerHTML = teamHTML;
+    })
+}
